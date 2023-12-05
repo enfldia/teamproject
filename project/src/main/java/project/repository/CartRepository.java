33b629267c9c913558df29a2dde47222bcdde7fb
enfldia <enfldia12@naver.com>
2023-12-05 12:23:38 +0900

    1205

---
+++ project/src/main/java/project/repository/CartRepository.java
@@ -0,0 +1,8 @@
+package project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.entity.Cart;
+
+public interface CartRepository extends JpaRepository<Cart,Long> {
+
+}
