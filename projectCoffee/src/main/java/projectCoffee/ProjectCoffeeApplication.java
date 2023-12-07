2e94696af20eeefca5f50db3b521f1f14de65243
enfldia <139440376+enfldia@users.noreply.github.com>
2023-12-07 11:22:01 +0900

    Add files via upload

---
+++ projectCoffee/src/main/java/projectCoffee/ProjectCoffeeApplication.java
@@ -0,0 +1,13 @@
+package projectCoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCoffeeApplication.class, args);
+	}
+
+}
---
+++ projectCoffee/src/test/java/projectCoffee/ProjectCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package projectCoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
